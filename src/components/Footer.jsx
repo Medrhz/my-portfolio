99a9545e9d82cc532705c2502e0d6576002912f3
RHZIZA Mohamed <109476153+Medrhz@users.noreply.github.com>
2025-12-25 10:06:08 +0100

    modify footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <footer className="py-10 text-center text-slate-400 text-sm border-t border-white/10">© {new Date().getFullYear()} Mohamed Rhziza — Built with ❤️ using React & Tailwind</footer>
+      <footer className="py-10 text-center text-slate-400 text-sm border-t border-white/10">© {new Date().getFullYear()} Mohamed Rhziza</footer>
     </div>
   );
 }
